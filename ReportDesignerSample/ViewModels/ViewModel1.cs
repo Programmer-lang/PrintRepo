a20e0fb7d52b7377c51f6a4169e9e6b9b96527e8
Programmer-lang <https://github.com/Programmer-lang/PrintRepo.git>
2020-04-23 22:59:29 +0300

    add DB To Folder

--- ReportDesignerSample/ViewModels/ViewModel1.cs
+++ ReportDesignerSample/ViewModels/ViewModel1.cs
@@ -7,6 +7,8 @@ using System.Drawing;
 using System.Drawing.Printing;
 using System.Windows;
 using ReportDesignerSample.Views;
+using DevExpress.DataAccess.Sql;
+using DevExpress.DataAccess.ConnectionParameters;
 
 namespace ReportDesignerSample.ViewModels
 {
@@ -29,13 +31,14 @@ namespace ReportDesignerSample.ViewModels
                     Margins = new Margins(100, 100, 100, 100)
                 };
 
-             
 
-                MyReport MyReport1 = new MyReport();
+                BindToData(report);
+                CreateReportHeader(report, "Print Data");
+                CreateDetail(report);
 
-                MyReport1.ReportDesigner.OpenDocument(report);
-
-                MyReport1.Show();
+                MyReport MyReport = new MyReport();
+                MyReport.ReportDesigner.OpenDocument(report);
+                MyReport.Show();
 
             }
             catch (Exception ex)
@@ -46,16 +49,55 @@ namespace ReportDesignerSample.ViewModels
             }
         }
 
+        public void BindToData(XtraReport report)
+        {
+            Model1 MyContext = new Model1();
+
+           string ConnectionString =  MyContext.Database.Connection.ConnectionString;
+            CustomStringConnectionParameters connectionParameters = new CustomStringConnectionParameters(ConnectionString);
+
+            DevExpress.DataAccess.Sql.SqlDataSource ds = new DevExpress.DataAccess.Sql.SqlDataSource(connectionParameters);
+
+            CustomSqlQuery QueryMaster = new CustomSqlQuery();
+            QueryMaster.Name = "Employes";
+            QueryMaster.Sql = "SELECT * FROM Employes";
+
+            CustomSqlQuery QueryChild = new CustomSqlQuery();
+            QueryChild.Name = "Orders";
+            QueryChild.Sql = "SELECT * FROM Orders";
+
+            ds.Queries.AddRange(new SqlQuery[] { QueryMaster, QueryChild });
+            ds.Relations.Add("QueryMaster", "QueryChild", "Id", "EmployeId");
+
+            report.DataSource = ds;
+            report.DataMember = "Employes";
+        }
+
+
+        public void CreateReportHeader(XtraReport report, string caption)
+        {
+
+            XRLabel Label = new XRLabel();
+            Label.Font = new Font("Tahoma", 12, System.Drawing.FontStyle.Bold);
+            Label.Text = caption;
+            Label.WidthF = 300F;
+
+            ReportHeaderBand ReportHeader = new ReportHeaderBand();
+            report.Bands.Add(ReportHeader);
+            ReportHeader.Controls.Add(Label);
+            ReportHeader.HeightF = Label.HeightF;
+        }
+
         public void CreateDetail(XtraReport report)
         {
 
-            // AppNum
+            // First Name
             XRLabel labelDetail = new XRLabel();
-            SetLabelProperties(labelDetail, "AppNum");
+            SetLabelProperties(labelDetail , "First Name", "FirstName");
 
-            // Note
+            // Last Name
             XRLabel labelDetail2 = new XRLabel();
-            SetLabelProperties(labelDetail2, "Note");
+            SetLabelProperties(labelDetail2 , "Last Name", "LastName");
 
             DetailBand detailBand = new DetailBand();
             detailBand.Height = labelDetail.Height;
@@ -74,7 +116,7 @@ namespace ReportDesignerSample.ViewModels
 
         }
 
-        public void SetLabelProperties(XRLabel label, String member)
+        public void SetLabelProperties(XRLabel label, String title, String member)
         {
 
             label.Font = new Font("Tahoma", 10, System.Drawing.FontStyle.Regular);
@@ -82,10 +124,20 @@ namespace ReportDesignerSample.ViewModels
             label.Borders = BorderSide.Left | BorderSide.Right | BorderSide.Bottom | BorderSide.Top;
             label.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleCenter;
             label.ExpressionBindings.Add(
-            new ExpressionBinding("BeforePrint", "Text", "'" + member.ToString() + ": ' + [" + member + "]"));
+            new ExpressionBinding("BeforePrint", "Text", "'" + title + ": ' + [" + member + "]"));
 
         }
 
+        public void CreateDetailReport(XtraReport report, string DataMember) 
+        {
+            DetailReportBand detailReportBand = new  DetailReportBand();
+            report.Bands.Add(detailReportBand);
+            detailReportBand.DataSource = report.DataSource;
+            detailReportBand.DataMember = DataMember;
+
+
+
+        }
     }
 
 
