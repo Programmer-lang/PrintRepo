66eefe358339f6d8bcb79424ae4b5cc3fbe44ff1
Programmer-lang <https://github.com/Programmer-lang/PrintRepo.git>
2020-04-24 18:23:45 +0300

    Print details

--- ReportDesignerSample/ViewModels/ViewModel1.cs
+++ ReportDesignerSample/ViewModels/ViewModel1.cs
@@ -35,6 +35,7 @@ namespace ReportDesignerSample.ViewModels
                 BindToData(report);
                 CreateReportHeader(report, "Print Data");
                 CreateDetail(report);
+                CreateDetailReport(report);
 
                 MyReport MyReport = new MyReport();
                 MyReport.ReportDesigner.OpenDocument(report);
@@ -53,7 +54,7 @@ namespace ReportDesignerSample.ViewModels
         {
             Model1 MyContext = new Model1();
 
-           string ConnectionString =  MyContext.Database.Connection.ConnectionString;
+            string ConnectionString = MyContext.Database.Connection.ConnectionString;
             CustomStringConnectionParameters connectionParameters = new CustomStringConnectionParameters(ConnectionString);
 
             DevExpress.DataAccess.Sql.SqlDataSource ds = new DevExpress.DataAccess.Sql.SqlDataSource(connectionParameters);
@@ -67,10 +68,10 @@ namespace ReportDesignerSample.ViewModels
             QueryChild.Sql = "SELECT * FROM Orders";
 
             ds.Queries.AddRange(new SqlQuery[] { QueryMaster, QueryChild });
-            ds.Relations.Add("QueryMaster", "QueryChild", "Id", "EmployeId");
+            ds.Relations.Add(QueryMaster.Name, QueryChild.Name, "Id", "EmployeId");
 
             report.DataSource = ds;
-            report.DataMember = "Employes";
+            report.DataMember = QueryMaster.Name;
         }
 
 
@@ -93,11 +94,11 @@ namespace ReportDesignerSample.ViewModels
 
             // First Name
             XRLabel labelDetail = new XRLabel();
-            SetLabelProperties(labelDetail , "First Name", "FirstName");
+            SetLabelProperties(labelDetail, "First Name", "FirstName");
 
             // Last Name
             XRLabel labelDetail2 = new XRLabel();
-            SetLabelProperties(labelDetail2 , "Last Name", "LastName");
+            SetLabelProperties(labelDetail2, "Last Name", "LastName");
 
             DetailBand detailBand = new DetailBand();
             detailBand.Height = labelDetail.Height;
@@ -121,26 +122,106 @@ namespace ReportDesignerSample.ViewModels
 
             label.Font = new Font("Tahoma", 10, System.Drawing.FontStyle.Regular);
             label.WidthF = 200F;
-            label.Borders = BorderSide.Left | BorderSide.Right | BorderSide.Bottom | BorderSide.Top;
-            label.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleCenter;
+            label.Borders = BorderSide.All;
+            label.BackColor = Color.LightBlue;
+            label.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleLeft;
             label.ExpressionBindings.Add(
             new ExpressionBinding("BeforePrint", "Text", "'" + title + ": ' + [" + member + "]"));
 
         }
 
-        public void CreateDetailReport(XtraReport report, string DataMember) 
+        public void CreateDetailReport(XtraReport report)
         {
-            DetailReportBand detailReportBand = new  DetailReportBand();
+            string DataMember;
+
+            SqlDataSource ds = report.DataSource as SqlDataSource;
+            DataMember = ds.Queries[0].Name + "." + ds.Relations[0].Name;
+
+            DetailReportBand detailReportBand = new DetailReportBand();
             report.Bands.Add(detailReportBand);
             detailReportBand.DataSource = report.DataSource;
             detailReportBand.DataMember = DataMember;
+          
+            // Add a header to the detail report.
+            ReportHeaderBand detailReportHeader = new ReportHeaderBand();
+            detailReportBand.Bands.Add(detailReportHeader);
+
+            XRTable tableHeader = new XRTable();
+            tableHeader.BeginInit();
+            tableHeader.Rows.Add(new XRTableRow());
+            tableHeader.Borders = BorderSide.All;
+            tableHeader.BorderColor = Color.DarkGray;
+            tableHeader.Font = new Font("Tahoma", 10, System.Drawing.FontStyle.Bold);
+            tableHeader.Padding = 10;
+            tableHeader.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleLeft;
+
+            XRTableCell cellHeader1 = new XRTableCell();
+            cellHeader1.Text = "Order No";
+            cellHeader1.BackColor = Color.LightGray;
+
+            XRTableCell cellHeader2 = new XRTableCell();
+            cellHeader2.Text = "Description";
+            cellHeader2.BackColor = Color.LightGray;
+
+            tableHeader.Rows[0].Cells.AddRange(new XRTableCell[] { cellHeader1 , cellHeader2 });
+            detailReportHeader.HeightF = tableHeader.HeightF;
+            detailReportHeader.Controls.Add(tableHeader);
+
+            // Adjust the table width.
+         //   tableHeader.BeforePrint += tableHeader_BeforePrint;
+            tableHeader.EndInit();
+
+            // Create a detail band.
+            XRTable tableDetail = new XRTable();
+            tableDetail.BeginInit();
+            tableDetail.Rows.Add(new XRTableRow());
+            tableDetail.Borders = BorderSide.Left | BorderSide.Right | BorderSide.Bottom;
+            tableDetail.BorderColor = Color.DarkGray;
+            tableDetail.Font = new Font("Tahoma", 10);
+            tableDetail.Padding = 10;
+            tableDetail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleLeft;
+
+            XRTableCell cellDetail1 = new XRTableCell();
+            XRTableCell cellDetail2 = new XRTableCell();
+
+            cellDetail1.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleLeft;
+            cellDetail2.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleLeft;
+
+            cellDetail1.ExpressionBindings.Add(new ExpressionBinding("Text", "[Id]"));
+            cellDetail2.ExpressionBindings.Add(new ExpressionBinding("Text", "[Description]"));
+
+            tableDetail.Rows[0].Cells.AddRange(new XRTableCell[] { cellDetail1, cellDetail2 });
+
+            DetailBand detailBand = new DetailBand();
+            detailBand.Height = tableDetail.Height;
+            detailReportBand.Bands.Add(detailBand);
+            detailBand.Controls.Add(tableDetail);
+
+            XRControlStyle oddStyle = new XRControlStyle();
+            XRControlStyle evenStyle = new XRControlStyle();
+
+            oddStyle.BackColor = Color.WhiteSmoke;
+            oddStyle.StyleUsing.UseBackColor = true;
+            oddStyle.Name = "OddStyle";
+
+            evenStyle.BackColor = Color.White;
+            evenStyle.StyleUsing.UseBackColor = true;
+            evenStyle.Name = "EvenStyle";
+
+            report.StyleSheet.AddRange(new XRControlStyle[] { oddStyle, evenStyle });
+            tableDetail.OddStyleName = oddStyle.Name;
+            tableDetail.EvenStyleName = evenStyle.Name;
+        }
 
 
-
+        void tableHeader_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e)
+        {
+            AdjustTableWidth(sender as XRTable);
+        }
+        private void AdjustTableWidth(XRTable table)
+        {
+            XtraReport report = table.RootReport;
+            table.WidthF = report.PageWidth - report.Margins.Left - report.Margins.Right;
         }
     }
-
-
-   
-
 }
